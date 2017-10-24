b218d41b6a9d7c1d047efbc9261ca0ff2b77ce4d
Matt Gibbs <mgibbs189@users.noreply.github.com>
2017-10-24 12:27:23 -0400

    Update google_maps.php

--- google_maps.php
+++ google_maps.php
@@ -51,7 +51,7 @@ class cfs_google_maps extends cfs_field
 
     function input_head( $field = null ) {
     ?>
-        <script src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=<?php echo defined( 'GMAPS_API_KEY' ) ? GMAPS_API_KEY : ''; ?>"></script>
     <?php
     }
 }
